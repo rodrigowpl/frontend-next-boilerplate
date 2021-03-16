b75dc6478fe0e955726573378658461238622b17
Rodrigo Wippel <rodrigow1992@gmail.com>
2021-03-16 07:57:50 -0300

    test husky three

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,6 @@ export default class MyDocument extends Document {
         })
 
       const initialProps = await Document.getInitialProps(ctx)
-
       return {
         ...initialProps,
         styles: (
